68737e6b0eaf535423d14ffd66f29f1757d8bfec
hackerainz <rainyraina23@gmail.com>
2020-11-25 03:30:28 +0800

    Added registration, login and profile features

--- frontend/register.php
+++ frontend/register.php
@@ -1,12 +1,7 @@
+<?php include('User.php') ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-
-
 <html>
+
     <head>
 
         <title>Singapore Tourism Attraction </title>
@@ -14,6 +9,8 @@ and open the template in the editor.
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- Bootstrap CSS only -->
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
               integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" 
               crossorigin="anonymous">
@@ -42,34 +39,241 @@ and open the template in the editor.
         <link href="css/cover.css" rel="stylesheet">
         <link href='css/register.css' rel="stylesheet">
     </head>
+
+
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        body {
+            background-color: #f1f1f1;
+        }
+
+        #regForm {
+            background-color: #ffffff;
+            margin: 100px auto;
+            font-family: Raleway;
+            padding: 40px;
+            width: 70%;
+            min-width: 300px;
+        }
+
+        h1 {
+            text-align: center;  
+        }
+
+        input {
+            padding: 10px;
+            width: 100%;
+            font-size: 17px;
+            font-family: Raleway;
+            border: 1px solid #aaaaaa;
+        }
+
+        /* Mark input boxes that gets an error on validation: */
+        input.invalid {
+            background-color: #ffdddd;
+        }
+
+        /* Hide all steps by default: */
+        .tab {
+            display: none;
+        }
+
+        button {
+            background-color: #4CAF50;
+            color: #ffffff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 17px;
+            font-family: Raleway;
+            cursor: pointer;
+        }
+
+        button:hover {
+            opacity: 0.8;
+        }
+
+        #prevBtn {
+            background-color: #bbbbbb;
+        }
+
+        /* Make circles that indicate the steps of the form: */
+        .step {
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbbbbb;
+            border: none;  
+            border-radius: 50%;
+            display: inline-block;
+            opacity: 0.5;
+        }
+
+        .step.active {
+            opacity: 1;
+        }
+
+        /* Mark the steps that are finished and valid: */
+        .step.finish {
+            background-color: #4CAF50;
+        }
+    </style>
     <body>
         <div class="signup-form">
-            <form action="/examples/actions/confirmation.php" method="post">
-                <h2>Register</h2>
-                <p class="hint-text">Create your account to book hotels.</p>
-                <div class="form-group">
-                    <input type="text" class="form-control" name="fname" placeholder="First Name">
+            <form action="register.php" method="post">
+                <?php include('errors.php'); ?>
+                <!-- One "tab" for each step in the form: -->
+                <div class="tab">
+                    <h2>Register</h2>
+                    <p class="hint-text">Create your account to book hotels.</p>
+                    <div class="form-group">
+                        <label for="fname">First Name:</label>
+                        <input type="text" class="form-control" name="fname" placeholder="First Name" id="fname" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="lname">Last Name:</label>
+                        <input type="text" class="form-control" name="lname" placeholder="Last Name" required="required" id="lname" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" name="email" placeholder="Email" required="required" id="email" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Phone number:</label>
+                        <input type="number" class="form-control" name="phone_number" placeholder="Phone Number" required="required" id="phone_number" required maxlength="8">
+                    </div>
+                    <div class="form-group">
+                        <label for="country">Country:</label>
+                        <input type="text" class="form-control" name="country" placeholder="Country" required="required" id="country" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="billing_address">Billing Address:</label>
+                        <input type="text" class="form-control" name="billing_address" placeholder="Billing Address" required="required" id="billing_address" required maxlength="200">
+                    </div>
                 </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" name="lname" placeholder="Last Name" required="required">
+               
+                <div class="tab">Login Information
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" class="form-control" name="username" placeholder="Username" required="required" id="username" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input type="password" class="form-control" name="pwd" placeholder="Password" required="required" id="pwd" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd_confirm">Confirm Password:</label>
+                        <input type="password" class="form-control" name="pwd_confirm" placeholder="Confirm Password" required="required" id="pwd_confirm" required maxlength="50">
+                    </div> 
                 </div>
-                <div class="form-group">
-                    <input type="email" class="form-control" name="email" placeholder="Email" required="required">
+
+
+
+
+                <div style="overflow:auto;">
+                    <div style="float:right;">
+                        <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+                        <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+                        <button type="submit" id="submitBtn" name="reg_user" display="none">Register</button>
+
+                    </div>
                 </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Password" required="required">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
-                </div>        
-                <div class="form-group">
-                    <label class="checkbox-inline"><input type="checkbox" required="required"> I accept the Terms of Use and Privacy Policy</a></label>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success btn-lg btn-block">Register Now</button>
+                <!-- Circles which indicates the steps of the form: -->
+                <div style="text-align:center;margin-top:40px;">
+                    <span class="step"></span>
+                    <span class="step"></span>
                 </div>
             </form>
             <div class="text-center">Already have an account? <a href="loginpage.php">Sign in</a></div>
         </div>
+        <script>
+            var currentTab = 0; // Current tab is set to be the first tab (0)
+            showTab(currentTab); // Display the current tab
+
+            function showTab(n) {
+                // This function will display the specified tab of the form...
+                var x = document.getElementsByClassName("tab");
+                x[n].style.display = "block";
+                //... and fix the Previous/Next buttons:
+                if (n == 0) {
+                    document.getElementById("prevBtn").style.display = "none";
+                    document.getElementById("submitBtn").style.display = "none";
+                    document.getElementById("nextBtn").style.display = "inline";
+                } else {
+                    document.getElementById("prevBtn").style.display = "inline";
+                }
+                if (n == (x.length - 1)) {
+                    document.getElementById("nextBtn").style.display = "none";
+                    document.getElementById("submitBtn").style.display = "inline";
+                } else {
+                    document.getElementById("nextBtn").innerHTML = "Next";
+                }
+
+                //... and run a function that will display the correct step indicator:
+                fixStepIndicator(n)
+            }
+
+            function nextPrev(n) {
+                // This function will figure out which tab to display
+                var x = document.getElementsByClassName("tab");
+                // Exit the function if any field in the current tab is invalid:
+                if (n == 1 && !validateForm())
+                    return false;
+                // Hide the current tab:
+                x[currentTab].style.display = "none";
+                // Increase or decrease the current tab by 1:
+                //if (n >3)
+                currentTab = currentTab + n;
+                // if you have reached the end of the form...
+                if (currentTab >= x.length) {
+                    // ... the form gets submitted:
+                    //document.getElementById("signup-form").submit();
+                    document.getElementById("element").style.display = "block";
+                    // return false;
+                }
+                // Otherwise, display the correct tab:
+                showTab(currentTab);
+            }
+
+            function validateForm() {
+                // This function deals with validation of the form fields
+                var x, y, i, valid = true;
+                x = document.getElementsByClassName("tab");
+                y = x[currentTab].getElementsByTagName("input");
+                // A loop that checks every input field in the current tab:
+                for (i = 0; i < y.length; i++) {
+                    // If a field is empty...
+                    if (y[i].value == "") {
+                        // add an "invalid" class to the field:
+                        y[i].className += " invalid";
+                        // and set the current valid status to false
+                        valid = false;
+                    }
+                }
+                // If the valid status is true, mark the step as finished and valid:
+                if (valid) {
+                    document.getElementsByClassName("step")[currentTab].className += " finish";
+                }
+                return valid; // return the valid status
+            }
+
+            function submitFunction() {
+                document.getElementById("submitBtn").style.display = "block";
+            }
+
+            function fixStepIndicator(n) {
+                // This function removes the "active" class of all steps...
+                var i, x = document.getElementsByClassName("step");
+                for (i = 0; i < x.length; i++) {
+                    x[i].className = x[i].className.replace(" active", "");
+                }
+                //... and adds the "active" class on the current step:
+                x[n].className += " active";
+            }
+
+        </script>
+
     </body>
 </html>
