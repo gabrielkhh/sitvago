6c6f0d3753acc511bd0af7a41b9b93af5144be35
Galaxy-power <su_jingxuan@hotmail.com>
2020-12-02 19:01:26 +0800

    Real Time

--- frontend/confirmation.php
+++ frontend/confirmation.php
@@ -92,17 +92,17 @@ $userBillingAddress = $_SESSION['billing_address'];
                     <div class="row">
                         <div class="col-50">
                             <label for="name"><i class="fa fa-user"></i> First Name</label>
-                            <input class="confirm_input" type="text" id="fnameDisabled" name="fnameDisabled" placeholder="Auto-Fill" value="<?= $userFName ?>" disabled>
-                            <input class="confirm_input" type="text" id="fname" name="fname" placeholder="Auto-Fill" value="<?= $userFName ?>" hidden>
+                            <label for="fnameDisabled"><input class="confirm_input" type="text" id="fnameDisabled" name="fnameDisabled" placeholder="Auto-Fill" value="<?= $userFName ?>" disabled></label>
+                            <label for="fname"><input class="confirm_input" type="text" id="fname" name="fname" placeholder="Auto-Fill" value="<?= $userFName ?>" hidden></label>
                             <label for="check-in-confirm"><i class="fa fa-calendar-check-o"></i> Check In Date</label>
-                            <input class="confirm_input" type="text" id="ci_dateDisabled" name="ci_dateDisabled" value="<?= $_POST['checkin'] ?>" disabled>
-                            <input class="confirm_input" type="text" id="ci_date" name="ci_date" value="<?= $_POST['checkin'] ?>" hidden>
+                            <label for="ci_dateDisabled"><input class="confirm_input" type="text" id="ci_dateDisabled" name="ci_dateDisabled" value="<?= $_POST['checkin'] ?>" disabled></label>
+                            <label for="ci_date"><input class="confirm_input" type="text" id="ci_date" name="ci_date" value="<?= $_POST['checkin'] ?>" hidden></label>
                             <label for="hotel-name"><i class="fa fa-building"></i> Hotel Name</label>
-                            <input class="confirm_input" type="text" id="hotelnameDisabled" name="hotelnameDisabled" value="<?= $_POST['hotelName'] ?>" disabled>
-                            <input class="confirm_input" type="text" id="hotel-name" name="hotel-name" value="<?= $_POST['hotelName'] ?>" hidden>
+                            <label for="hotelnameDisabled"><input class="confirm_input" type="text" id="hotelnameDisabled" name="hotelnameDisabled" value="<?= $_POST['hotelName'] ?>" disabled></label>
+                            <label for="hotel-name"><input class="confirm_input" type="text" id="hotel-name" name="hotel-name" value="<?= $_POST['hotelName'] ?>" hidden></label>
                             <label for="email"><i class="fa fa-envelope"></i> Email</label>
-                            <input class="confirm_input" type="text" id="emailDisabled" name="emailDisabled" placeholder="Auto-Fill" required name="email" value="<?= $userEmail ?>" disabled>
-                            <input class="confirm_input" type="text" id="email" name="email" placeholder="Auto-Fill" required name="email" value="<?= $userEmail ?>" hidden>
+                            <label for="emailDisabled"><input class="confirm_input" type="text" id="emailDisabled" name="emailDisabled" placeholder="Auto-Fill" required name="email" value="<?= $userEmail ?>" disabled></label>
+                            <label for="email"><input class="confirm_input" type="text" id="email" name="email" placeholder="Auto-Fill" required name="email" value="<?= $userEmail ?>" hidden></label>
                             <label for="card-element">
                                 Credit Or Debit Card
                                 <i class="fa fa-cc-visa" style="color:navy;"></i>
@@ -119,23 +119,23 @@ $userBillingAddress = $_SESSION['billing_address'];
                         </div>
                         <div class="col-50">
                             <label for="city"><i class="fa fa-user"></i> Last Name</label>
-                            <input class="confirm_input" type="text" id="lnameDisabled" name="lnameDisabled" placeholder="Auto-Fill" value="<?= $userLName ?>" disabled> 
-                            <input class="confirm_input" type="text" id="lname" name="lname" placeholder="Auto-Fill" value="<?= $userLName ?>" hidden>                                        
+                            <label for="lnameDisabled"><input class="confirm_input" type="text" id="lnameDisabled" name="lnameDisabled" placeholder="Auto-Fill" value="<?= $userLName ?>" disabled></label>
+                            <label for="lname"><input class="confirm_input" type="text" id="lname" name="lname" placeholder="Auto-Fill" value="<?= $userLName ?>" hidden></label>                                        
                             <label for="check-out-confirm"><i class="fa fa-calendar-check-o"></i> Check Out Date</label>
-                            <input class="confirm_input" type="text" id="codateDisabled" name="codateDisabled" value="<?= $_POST['checkout'] ?>" disabled>
-                            <input class="confirm_input" type="text" id="co_date" name="co_date" value="<?= $_POST['checkout'] ?>" hidden>
+                            <label for="codateDisabled"><input class="confirm_input" type="text" id="codateDisabled" name="codateDisabled" value="<?= $_POST['checkout'] ?>" disabled></label>
+                            <label for="co_date"><input class="confirm_input" type="text" id="co_date" name="co_date" value="<?= $_POST['checkout'] ?>" hidden></label>
                             <label for="room-type"><i class="fa fa-bed"></i> Room Type</label>
-                            <input class="confirm_input" type="text" id="roomtypeDisabled" name="roomtypeDisabled" value="<?= $_POST['TypeOfRooms'] ?>" disabled>
-                            <input class="confirm_input" type="text" id="room-type" name="room-type" value="<?= $_POST['TypeOfRooms'] ?>" hidden>                                       
+                            <label for="roomtypeDisabled"><input class="confirm_input" type="text" id="roomtypeDisabled" name="roomtypeDisabled" value="<?= $_POST['TypeOfRooms'] ?>" disabled></label>
+                            <label for="room-type"><input class="confirm_input" type="text" id="room-type" name="room-type" value="<?= $_POST['TypeOfRooms'] ?>" hidden></label>                                       
                             <label for="adr"><i class="fa fa-address-card-o"></i> Billing Address</label>
-                            <input class="confirm_input" type="text" id="adrDisabled" name="addressDisabled" placeholder="Auto-Fill" value="<?= $userBillingAddress ?>" disabled>
-                            <input class="confirm_input" type="text" id="adr" name="address" placeholder="Auto-Fill" value="<?= $userBillingAddress ?>" hidden>
+                            <label for="addressDisabled"><input class="confirm_input" type="text" id="adrDisabled" name="addressDisabled" placeholder="Auto-Fill" value="<?= $userBillingAddress ?>" disabled></label>
+                            <label for="address"><input class="confirm_input" type="text" id="adr" name="address" placeholder="Auto-Fill" value="<?= $userBillingAddress ?>" hidden></label>
                             <label for="notice"><i class="fa fa-exclamation-circle">Notice!</i></label>
                             <p id="notice">Do check the price before confirming!</p>
                             
                         </div>                           
                     </div>
-                    <input type="submit" value="Confirm Booking" class="btn" name="confirm_book_btn" id="confirm_book_btn">
+                    <label for="confirm_book_btn"><input type="submit" value="Confirm Booking" class="btn" name="confirm_book_btn" id="confirm_book_btn"></label>
                 </form>
             </div>
         </div>
