933354c0eed489e53f5857f9d38ef848c9cd03d1
hackerainz <rainyraina23@gmail.com>
2020-12-04 03:17:21 +0800

    i didnt kill the nav bar okkkk

--- frontend/navbar_User.php
+++ frontend/navbar_User.php
@@ -2,24 +2,30 @@
     .nav-link{
         color: rgb(186,227,222) !important;
     }
+    .nav-link-active{
+        color: rgb(140, 124, 250) !important;
+    }
     
     .bg-dark{
         background-color: #1f2022 !important;
     }
+    
+    .navbar-brand img{
+        width:100px;
+        height: 35px;
+    }
 </style>
 
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" aria-label="Primary navigation">
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
 
     <nav>
         <a class="navbar-brand" href="home.php">
-            <img src="images/logo_nobackground.png" width="100" height="35" class="d-inline-block align-top" alt="sitvago logo" > 
+            <img src="images/logo_nobackground.png" class="d-inline-block align-top" alt="logo for navbar"> 
         </a>
     </nav>
-
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
