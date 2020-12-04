48bd8fe73715cc5877281c33692e15790a53bd8e
Gabriel Kok <gabrielkok4227@gmail.com>
2020-12-04 22:21:34 +0800

    Fix for footer

--- frontend/footer.php
+++ frontend/footer.php
@@ -1,14 +1,7 @@
-<style>
-    footer a{   
-        color:black;
-        font-weight:bold;
-    }
-</style>
-
 <footer class="container">
     <div style="text-align:center;">
         <p>&copy; Sitvago 2020</p>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Learn more about Sitvago and the team behind it.</a>
+        <a style="color:black; font-weight:bold;" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Learn more about Sitvago and the team behind it.</a>
     </div>
 </footer>
 
