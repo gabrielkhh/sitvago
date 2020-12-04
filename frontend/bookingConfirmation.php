f158e25b226eb7342fb216a4c3136801ef4c6ea9
Galaxy-power <72615774+Galaxy-power@users.noreply.github.com>
2020-12-04 16:32:54 +0800

    wc3 fix, think navbar/footer got some style thing

--- frontend/bookingConfirmation.php
+++ frontend/bookingConfirmation.php
@@ -45,7 +45,7 @@ unset($_SESSION['INFO']);
     <div class="container my-card">
         <h2 id="card-h2">Thank you for using Sitvago</h2>
         <div class="card">
-            <img class="card-img-top" src="images/logo_nobackground.png" alt="Card image"></img>
+            <img class="card-img-top" src="images/logo_nobackground.png" alt="Card image">
             <div class="card-body">
                 <hr class="line">                    
                     <p class="card-text">
@@ -67,9 +67,6 @@ unset($_SESSION['INFO']);
             </div>
         </div>
     </div>
-		<?php
-			include "footer.php";
-		?>
 </body>
 
 
